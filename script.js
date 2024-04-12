3ccbb12229d2349609e6127cde4ec85a95becbb9
AidaxoIQ <166862200+AidaxoIQ@users.noreply.github.com>
2024-04-12 21:53:12 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let score = 0;
-
-const coin = document.getElementById('coin');
-const scoreDisplay = document.getElementById('score');
-
-coin.addEventListener('click', () => {
-    score++;
-    scoreDisplay.textContent = score;
-});
